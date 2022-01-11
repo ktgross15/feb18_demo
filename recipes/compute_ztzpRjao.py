e04bfdb7cec27c30a80643cbcbda7b781c57676e
ktgross15 <katie.gross@dataiku.com>
2022-01-11 15:23:38 -0500

    Recipe CREDITCARDFRAUD.compute_ztzpRjao saved through notebook notebook editor for compute_ztzpRjao

--- recipes/compute_ztzpRjao.py
+++ recipes/compute_ztzpRjao.py
@@ -1,15 +1,28 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
-from dataiku import pandasutils as pdu
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read recipe inputs
 transactions_known = dataiku.Dataset("transactions_known")
-transactions_known_df = transactions_known.get_dataframe()
+df = transactions_known.get_dataframe(limit=10000)
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df.head()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+model = RandomForestClassifier()
+model.fit(X, y)
 
+today = datetime.date.today()
+filename = 'model_{}-{}-{}.pkl'.format(today.year, today.month, today.day)
 
+model_folder = dataiku.Folder("model_folder")
+with model_folder.get_writer(filename) as w:
+    pickle.dump(model, w)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 model_folder = dataiku.Folder("ztzpRjao")
 model_folder_info = model_folder.get_info()
