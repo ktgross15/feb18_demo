964c5ff2f30397178e026228caffbfc82fa5f181
ktgross15 <katie.gross@dataiku.com>
2022-02-07 16:51:08 -0500

    Updated Lambda service config

--- lambda_services/fraud_service/endpoint-python_preds.py
+++ lambda_services/fraud_service/endpoint-python_preds.py
@@ -1,6 +1,6 @@
 from  dataiku.apinode.predict.predictor import ClassificationPredictor
 import pandas as pd
-from cPickle import pickle
+import pickle
 import os
 
 class MyPredictor(ClassificationPredictor):
@@ -8,7 +8,7 @@ class MyPredictor(ClassificationPredictor):
     def __init__(self, data_folder = None):
         self.data_folder = data_folder
         model_file = os.path.join(self.data_folder,'model.pkl')
-        self.model = pickle.load(open(model_file,'rb'))
+        self.model = pickle.load(open(model_file,'r'))
 
     def predict(self, features_df):
         model = self.model
