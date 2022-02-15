bab2fa5139c475eb54abc4fe662e52488dbf3929
ktgross15 <katie.gross@dataiku.com>
2022-02-15 08:35:45 -0500

    Saved recipe: CREDITCARDFRAUD.compute_transactions_R

--- recipes/compute_transactions_R.r
+++ recipes/compute_transactions_R.r
@@ -1,7 +1,8 @@
 library(dataiku)
 
 # Recipe inputs
-transactions_stacked_prepared <- dkuReadDataset("transactions_stacked_prepared", samplingMethod="head", nbRows=100000, inferColClassesFromData = FALSE)
+transactions_stacked_prepared <- dkuReadDataset("transactions_stacked_prepared", samplingMethod="head", nbRows=100000, 
+                                                inferColClassesFromData = FALSE)
 
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a R dataframe or data table
